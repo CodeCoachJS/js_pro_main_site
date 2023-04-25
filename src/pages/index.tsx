cb2b54e78656778fa866fc6868b60de532dba2b7
Brian Jenney <brianjenney@Brians-MacBook-Pro.local>
2023-04-24 18:27:30 -0700

    update site to include categories

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,57 @@
 import { type NextPage } from "next";
-
+import { useEffect, useState, useMemo } from "react";
 import Head from "next/head";
-
 import { signIn, signOut, useSession } from "next-auth/react";
-
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const { data: sessionData } = useSession();
 
   const { data } = api.repos.getRepos.useQuery();
+  const [repos, setRepos] = useState(data);
+  const [filter, setFilter] = useState<Set<string>>(new Set());
+
+  const categories = useMemo(() => {
+    const categories = new Set<string>();
+    data?.forEach((repo) => {
+      if (repo.categories) {
+        repo.categories.forEach((cat) => categories.add(cat));
+      }
+    });
+    return categories;
+  }, [data]);
+
+  useEffect(() => {
+    setRepos(data);
+  }, [data]);
+
+  useEffect(() => {
+    if (filter.size > 0) {
+      setRepos(
+        data?.filter((repo) => {
+          if (repo.categories) {
+            return repo.categories.some((cat) => filter.has(cat));
+          }
+          return false;
+        })
+      );
+    } else {
+      setRepos(data);
+    }
+  }, [filter, data]);
+
+  const filterByCategory = (category: string) => {
+    if (category === "all") {
+      setFilter(new Set());
+      return;
+    }
+    if (filter.has(category)) {
+      filter.delete(category);
+    } else {
+      filter.add(category);
+    }
+    setFilter(new Set(filter));
+  };
 
   return (
     <>
@@ -18,14 +60,14 @@ const Home: NextPage = () => {
         <meta name="description" content="JS Pros repos" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-dark-blue min-h-screen">
+      <main className="min-h-screen">
         <h1 className="text-center text-4xl text-white">JS Pros</h1>
-        <button
+        {/* <button
           className="bg-purple hover:bg-purple-lighter rounded-full px-4 py-2 font-semibold no-underline transition"
           onClick={sessionData ? () => void signOut() : () => void signIn()}
         >
           {sessionData ? "Sign out" : "Sign in"}
-        </button>
+        </button> */}
         <p className="text-center text-2xl text-white">
           {sessionData && (
             <span className="text-blue-lighter">
@@ -33,6 +75,32 @@ const Home: NextPage = () => {
             </span>
           )}
         </p>
+        <div className="flex flex-wrap justify-center">
+          <button
+            key="all"
+            className={`${
+              filter.has("all")
+                ? "bg-purple-700"
+                : "bg-purple-500 hover:bg-purple-600"
+            } mb-2 mr-2 flex items-center rounded-full px-4 py-2 font-semibold text-white no-underline transition`}
+            onClick={() => filterByCategory("all")}
+          >
+            All
+          </button>
+          {Array.from(categories).map((cat) => (
+            <button
+              key={cat}
+              className={`${
+                filter.has(cat)
+                  ? "bg-purple-700"
+                  : "bg-purple-500 hover:bg-purple-600"
+              } mb-2 mr-2 flex items-center rounded-full px-4 py-2 font-semibold text-white no-underline transition`}
+              onClick={() => filterByCategory(cat)}
+            >
+              {cat}
+            </button>
+          ))}
+        </div>
         <div
           className="flex flex-wrap justify-center space-x-4 space-y-4"
           style={{
@@ -41,7 +109,7 @@ const Home: NextPage = () => {
             justifyContent: "center",
           }}
         >
-          {data?.map((repo) => (
+          {repos?.map((repo) => (
             <a
               key={repo.id}
               href={repo.url}
--- src/server/api/routers/repos.ts
+++ src/server/api/routers/repos.ts
@@ -1,7 +1,13 @@
-import { supabase } from "~/pages/api/trpc/[trpc]";
 import type { PostgrestResponse } from "@supabase/supabase-js";
+import { supabase } from "~/pages/api/trpc/[trpc]";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+interface Category {
+  category: {
+    name: string;
+  };
+}
 
 interface Repo {
   id: number;
@@ -10,17 +16,45 @@ interface Repo {
   description: string;
   updated_at: string;
   isPrivate: boolean;
+  repo_categories: Category[];
+}
+
+interface RepoWithCategories extends Repo {
+  categories: string[];
 }
 
 export const repos = createTRPCRouter({
-  getRepos: publicProcedure.query(async (): Promise<Repo[]> => {
-    const { data, error }: PostgrestResponse<unknown> = await supabase
+  getRepos: publicProcedure.query(async (): Promise<RepoWithCategories[]> => {
+    const { data, error }: PostgrestResponse<Repo> = await supabase
       .from("repos")
-      .select("*");
+      .select(
+        `
+      id,
+      name,
+      url,
+      description,
+      updated_at,
+      isPrivate,
+      repo_categories (
+        category: categories ( name )
+      )
+    `
+      );
 
     if (error) {
       throw error;
     }
-    return data as Repo[];
+
+    const transformedData: RepoWithCategories[] =
+      data?.map((repo): RepoWithCategories => {
+        const categories: string[] = repo.repo_categories.map(
+          (category: Category) => {
+            return category.category.name;
+          }
+        );
+        return { ...repo, categories } as RepoWithCategories;
+      }) || [];
+
+    return transformedData;
   }),
 });
