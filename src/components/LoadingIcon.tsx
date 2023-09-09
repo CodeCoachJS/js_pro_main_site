a8708d71a92c59941c59ec0187396c4aa83ada30
Louie Knolle <knollelw@gmail.com>
2023-09-09 16:54:16 -0400

    adjust position and gradient of loading icon

--- src/components/LoadingIcon.tsx
+++ src/components/LoadingIcon.tsx
@@ -2,9 +2,9 @@ import { type NextComponentType } from "next";
 
 const LoadingIcon: NextComponentType = () => {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="relative h-24 w-24 animate-spin rounded-full bg-gradient-to-r from-red-400 via-green-400 via-indigo-500 via-orange-300 to-blue-400 to-purple-500 to-yellow-300">
-        <div className="absolute left-1/2 top-1/2 h-20 w-20 -translate-x-1/2 -translate-y-1/2 transform rounded-full border-2 border-white bg-gray-200"></div>
+    <div className="flex h-screen justify-center">
+      <div className="relative h-24 w-24 animate-spin rounded-full bg-gradient-to-r from-blue-500 to-purple-500">
+        <div className="bg-card-bg bg-card-bg absolute left-1/2 top-1/2 h-20 w-20 -translate-x-1/2 -translate-y-1/2 transform rounded-full border-2 border-white bg-white"></div>
       </div>
     </div>
   );
