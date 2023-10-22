457a1340a568ed9e52d151547cd546302acbdf21
paulbmun <pmun831@gmail.com>
2023-10-22 14:06:13 -0400

    auto_add_feature wip

--- src/pages/api/memberships.ts
+++ src/pages/api/memberships.ts
@@ -1,34 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+// import { createAppAuth } from "@octokit/auth-app";
+// import { Octokit } from "@octokit/rest";
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { env } from "~/env.mjs";
+import { env } from "../../env.mjs";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req : NextApiRequest, res : NextApiResponse) {
   if (req.method === 'POST') {
-    try {
-        const { email } = req.body as { email: string };
-        const githubRes = await sendGitHubInvite(email);
+    const { email } = req.body;
 
-        if (githubRes.status == 200) {
-            res.status(200).json({ message: "Invitation sent successfully. "});
-        } else {
-            res.status(githubRes.status).json({ message: 'Failed to send invite.' });
-        }
-    } catch (error) {
-        console.error('Error sending GitHub invitation:', error);
-        res.status(500).json({ message: 'Internal server error' });
-      }
-  } else {
-    res.status(405).json({ message: 'Method not allowed.' });
+    if (req.headers.key === env.CODECOACH_KEY) {
+      return res.json('correct key')
+    }
+
+    console.log(req.headers)
+    return res.json('THE error')
+
+
   }
-}
+   
 
-async function sendGitHubInvite(email: string) {
-    const response = await fetch(env.GITHUB_API_URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: env.GITHUB_PERSONAL_TOKEN,
-        },
-        body: JSON.stringify({ email }),
-    });
-    return response;
-}
+  }
+
+      // const response = await octokit.request('POST /orgs/CodeCoachJS/invitations', {
+      //   org: 'CodeCoachJS',
+      //   email,
+      //   role: 'direct_member',
+      //   request: {
+      //     headers: {
+      //       'X-GitHub-Api-Version': '2022-11-28',
+      //     },
+      //   },
+      // });
+
+    //   return res.send({
+    //     data: response.data,
+    //   });
+    // } catch (error) {
+    //   console.error("Error:", error);
+    //   return res.status(500).json({ error: "An error occurred" });
+  //   }
+  // }
+
+
+
+
+
