f0368892d932bd15807189fd81ead87a1da88ce0
paulbmun <pmun831@gmail.com>
2023-11-05 12:04:34 -0500

    auto add members features on memberships route

--- src/pages/api/memberships.ts
+++ src/pages/api/memberships.ts
@@ -1,48 +1,44 @@
+/* eslint-disable @typescript-eslint/require-await */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-// import { createAppAuth } from "@octokit/auth-app";
-// import { Octokit } from "@octokit/rest";
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
 import { env } from "../../env.mjs";
 
-export default async function handler(req : NextApiRequest, res : NextApiResponse) {
-  if (req.method === 'POST') {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
     const { email } = req.body;
 
-    if (req.headers.key === env.CODECOACH_KEY) {
-      return res.json('correct key')
+    const githubToken = env.GITHUB_PERSONAL_TOKEN;
+
+    if (req.headers.key !== env.CODECOACH_KEY) {
+      return res.status(401).json("Invalid key");
     }
 
-    console.log(req.headers)
-    return res.json('THE error')
+    const config = {
+      method: "POST",
+      headers: {
+        Authorization: `token ${githubToken}`,
+      },
+      body: JSON.stringify({
+        role: "direct_member",
+        email,
+      }),
+    };
 
+    try {
+      const request = await fetch(env.GITHUB_API_URL, config);
+      const response = await request.json();
+      console.log(response);
 
+      return res.status(200).json({ message: `${email as string} added` });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json("An error occurred while adding the user to CodeCoachJS");
+    }
   }
-   
-
-  }
-
-      // const response = await octokit.request('POST /orgs/CodeCoachJS/invitations', {
-      //   org: 'CodeCoachJS',
-      //   email,
-      //   role: 'direct_member',
-      //   request: {
-      //     headers: {
-      //       'X-GitHub-Api-Version': '2022-11-28',
-      //     },
-      //   },
-      // });
-
-    //   return res.send({
-    //     data: response.data,
-    //   });
-    // } catch (error) {
-    //   console.error("Error:", error);
-    //   return res.status(500).json({ error: "An error occurred" });
-  //   }
-  // }
-
-
-
-
-
+}
