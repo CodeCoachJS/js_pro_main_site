8a73cbc04e022a2d671375ddaf465e9217bedd44
Brian Jenney <brianjenney83@gmail.com>
2024-04-23 10:10:57 -0700

    update home page and add script for affiliates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,38 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
+import Script from "next/script";
 import { SessionProvider } from "next-auth/react";
 import Header from "~/components/Header";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+/**
+ * 
+ * @param param0 
+ * 
+</script>
+ * @returns 
+ */
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Header />
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Script
+        id="affiliates"
+        async
+        src="//cdn.trackdesk.com/tracking.js"
+        dangerouslySetInnerHTML={{
+          __html: `(function(t,d,k){(t[k]=t[k]||[]).push(d);t[d]=t[d]||t[k].f||function(){(t[d].q=t[d].q||[]).push(arguments)}})(window,"trackdesk","TrackdeskObject");trackdesk('javascriptprosapp', 'click');`,
+        }}
+      />
+      <SessionProvider session={session}>
+        <Header />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,6 @@ import { getServerAuthSession } from "../server/auth";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const session = await getServerAuthSession(ctx);
-
   if (session?.isMember) {
     return {
       redirect: {
@@ -24,72 +23,105 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center gap-8 bg-black text-white">
+    <div className="flex min-h-screen flex-col items-center bg-gray-900 text-white">
       <Head>
         <title>Not Another Course</title>
-        <meta name="description" content="A set of challenges for JS developers" />
+        <meta
+          name="description"
+          content="A set of challenges for JS developers"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="mx-auto flex max-w-4xl flex-col items-center gap-8 p-4">
-        <div className="flex flex-col items-center gap-8">
-          <h1 className="mb-4 mt-20 text-center text-4xl font-bold leading-[1.2em] md:text-6xl">
-            <span className="text-white">Practice Solving</span>
-            <span className="bg-gradient-to-l from-indigo-500 to-pink-500 bg-clip-text text-transparent">
-              <br />
-              Complex Problems.
-            </span>
-          </h1>
+      <main className="mx-auto flex w-full max-w-4xl flex-col items-center gap-8 p-4">
+        <h1 className="mb-4 mt-20 text-center text-4xl font-bold md:text-6xl">
+          Practice Solving
+          <span className="block bg-gradient-to-l from-indigo-500 to-pink-500 bg-clip-text text-transparent">
+            Complex Problems.
+          </span>
+        </h1>
+        <h2 className="block bg-gradient-to-l from-indigo-500 to-pink-500 bg-clip-text text-transparent">
+          You graudated the bootcamp. Now what?
+        </h2>
 
-          <Image
-            width={700}
-            height={394}
-            alt="Video Screenshot"
-            src="/challenges.png"
-            className="rounded-lg shadow-xl"
-          />
+        <Image
+          width={700}
+          height={394}
+          alt="Video Screenshot"
+          src="/challenges.png"
+          className="rounded-lg shadow-xl"
+        />
 
-          <p className="max-w-xl text-center text-xl leading-relaxed text-white md:text-2xl">
-            Learn tough stuff like Webpack, Redux, TypeScript, Unit Testing, Serverless and more. I developed this material for JavaScript developers who want to level up their skills with real world challenges that mimic what you will see on the job.
-          </p>
+        <p className="max-w-xl text-center text-xl md:text-2xl">
+          Learn tough stuff like Webpack, Redux, TypeScript, Unit Testing,
+          Serverless and more. This material is developed for JavaScript
+          developers who want to level up their skills with real-world
+          challenges that mimic what you WILL see on the job.
+        </p>
 
-          <Image
-            width={700}
-            height={394}
-            alt="Happy student"
-            src="/paul_testimonial.png"
-            className="rounded-lg shadow-xl my-4"
-          />
+        <Image
+          width={700}
+          height={394}
+          alt="Happy student"
+          src="/paul_testimonial.png"
+          className="my-4 rounded-lg shadow-xl"
+        />
 
-          <p className="max-w-xl text-center text-xl leading-relaxed text-white md:text-2xl">
-            Practice debugging pipelines. Publish your first NPM library. Write your first line of TypeScript and more. Learn DSA in a practical, hands-on way to crush the interview. This is the course I wish I had when I switched careers.
-          </p>
+        <p className="max-w-xl text-center text-xl md:text-2xl">
+          Practice debugging pipelines, publish your first NPM library, write
+          your first line of TypeScript, and more. Learn DSA in a practical,
+          hands-on way to crush the interview. This is the course I wish I had
+          when I switched careers.
+        </p>
 
-          <Image
+        <Image
           width={400}
           height={400}
           alt="lambda challenge"
           src="/group_shot.png"
-          />
+          className="rounded-lg shadow-xl"
+        />
 
-          <p className="max-w-xl text-center text-xl leading-relaxed text-white md:text-2xl">
-            {`Get access to a private Slack community where you can ask questions, share your progress, and get help from other developers. This is a great way to network and make friends in the industry. An entire section is dedicated to career advice and job hunting. I want to help you land your dream job and also help you navigate the industry. I've been a developer for over 10 years and I've seen it all. I want to help you avoid the pitfalls and land your dream job`}
-          </p>
+        <p className="max-w-xl text-center text-xl md:text-2xl">
+          Get access to a private Slack community where you can ask questions,
+          share your progress, and get help from other developers. This is a
+          great way to network and make friends in the industry. An entire
+          section is dedicated to career advice and job hunting.{" "}
+        </p>
 
-          <div>
-            <Link
-              href={env.NEXT_PUBLIC_STRIPE_URL}
-              className="inline-flex items-center justify-center rounded-full bg-gradient-to-r from-indigo-500 to-pink-500 px-6 py-3 font-bold text-white transition-all hover:from-indigo-400 hover:to-pink-400"
-            >
-              Join the team →
-            </Link>
-          </div>
-        </div>
+        <p className="max-w-xl text-center text-xl md:text-2xl">
+          {" "}
+          I want to help you land your dream job and navigate the industry. I've
+          been a developer for over 10 years. I want to help you avoid the
+          pitfalls and mistakes I made.
+        </p>
 
-        <footer className="mt-8 p-4 text-center text-gray-500">
-          <p>&copy; 2024 Not Another Course. All rights reserved.</p>
-        </footer>
+        <Image
+          width={800}
+          height={800}
+          alt="best course"
+          src="/ali_testimonial.png"
+          className="rounded-lg shadow-xl"
+        />
+
+        <p className="max-w-xl text-center text-xl md:text-2xl">
+          No subscription fees. Just one low price and it is yours for life. The
+          Slack access is worth the price alone. You are going to learn a ton
+          and have a great time doing it. I can not wait to see you in the
+          community.
+        </p>
+
+        <Link
+          href={env.NEXT_PUBLIC_STRIPE_URL}
+          className="rounded-full bg-gradient-to-r from-indigo-500 to-pink-500 px-6 py-3 font-bold text-white transition-colors hover:from-indigo-400 hover:to-pink-400"
+        >
+          Start Building →
+        </Link>
       </main>
+
+      <footer className="p-4 text-center text-gray-500">
+        <p>&copy; 2024 Not Another Course. All rights reserved.</p>
+      </footer>
     </div>
   );
 };
